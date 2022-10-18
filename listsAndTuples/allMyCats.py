7aa1e6dc418735ca151f80d93ac49f9b00c32aa4
duckCuddlerTadashi <aahashish100@gmail.com>
2022-10-18 23:04:56 +0545

    lists and its cousin tuples

---
+++ listsAndTuples/allMyCats.py
@@ -0,0 +1,8 @@
+catName1 = input('Enter the name of cat 1:')
+catName2 = input('Enter the name of cat 2:')
+catName3 = input('Enter the name of cat 3:')
+catName4 = input('Enter the name of cat 4:')
+catName5 = input('Enter the name of cat 5:')
+catName6 = input('Enter the name of cat 6:')
+print('The name of cats are:')
+print(catName1+' '+catName2+' '+catName3+' '+catName4+' '+catName5+' '+catName6)
